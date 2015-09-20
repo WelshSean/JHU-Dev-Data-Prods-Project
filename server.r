bfe4d5bc81bb870c61c03177cd91eab5247f96fb
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-09-20 21:25:45 +0100

    Added a validate clause - very happy :-)

--- server.r
+++ server.r
@@ -4,15 +4,14 @@ shinyServer(
   function(input, output){
     output$oid1 <- renderText({input$idcvs})
     output$oid2 <- renderText({paste(input$idcvs, collapse = "+")})
-     #model1  <- reactive({
-     #input$calcbutton
-     #isolate(lm(mpg ~ paste(input$idcvs, collapse = "+")   , mtcars))
-      # isolate(lm(mpg ~ cyl   , mtcars))
-      #      })
+ 
     model1 <- eventReactive(input$calcbutton, {
-      #paste("ZZZ", "YYY", "XXX")
+      validate(
+        need(input$idcvs != "", "Please select at least one Covariant")
+      )
       lm(paste("mpg ~", paste(input$idcvs, collapse = "+"))   , mtcars)
     })
+
     output$oid3 <- renderText({model1()$coefficients})
   }
 )
