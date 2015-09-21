73af79cc632669161d82f57e6392ffd7a882122b
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-09-21 22:05:29 +0100

    Updated reactive code to improve event handling so that behaviour is more consistent. Validate now fires and NOTHING happens unless the button is pressed

--- server.r
+++ server.r
@@ -13,28 +13,29 @@ shinyServer(
       )
       lm(paste("mpg ~", paste(input$idcvs, collapse = "+"))   , mtcars)
       
-      #output$oid3 <- renderText({model1()$coefficients})
     })
-
+    observe({
+      if(input$calcbutton > 0){
     output$oid3 <- renderText({
-      if (is.null(input$idcvs)) return()
-      #model1()$coefficients
       oo <- NULL
       for (i in names(model1()$coefficients)){
         oo <- c(oo,paste(c(i,model1()$coefficients[i]),collapse = ": "))
       }
       oo
       })
+ # }
+  #  })
 
     
     output$oplot <- renderPlot({
-      if (is.null(input$idcvs)) return()
+      #if (is.null(input$idcvs)) return()
       par(mfrow = c(2, 2))
       plot(model1(), which = 1:4, caption = c("Residuals vs Fitted", "Normal Q-Q plot", "Scale-Location plot", 
                           "Cook's distance plot"), panel = points, sub.caption = deparse(model1()$call), main = "",
                           ask = prod(par("mfcol")) < length(which) && dev.interactive(),
            id.n = 3, labels.id = names(residuals(model1())), cex.id = 0.75)
-      #plot(mtcars$mpg,mtcars$cyl)
     })
+      }
+    })#
   }
 )
